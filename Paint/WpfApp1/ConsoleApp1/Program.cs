545686bb621882409766255b3a932facfd1ce9fd
Kate <k.y.bondarenko@gmail.com>
2019-04-03 12:11:38 +0300

    Paint3

---
+++ Paint/WpfApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            do
+            {
+                Console.WriteLine("Hello");
+                a++;
+            }
+
+            while (a<11);
+        }
+    }
+}
